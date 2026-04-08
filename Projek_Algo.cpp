906ca3a38e973ce001cff2c41cd5ff6dba878dbd
audreyfrisheila <124250120@student.upnyk.ac.id>
2026-04-08 12:45:36 +0700

    by audrey

--- Projek_Algo.cpp
+++ Projek_Algo.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-    cout<<"Hello World"<<endl;
-    return 0;
+
+
+struct dataBuku{
+    char id_buku[10];
+    char judul_buku[100];
+    int stok_buku;
+    int total_peminjaman;
+    int tersedia;
+    int terpinjam;
+    
+}; dataBuku buku[100];
+
+struct peminjamanBuku{
+    char id_buku[10];
+    char id_peminjam[10];
+    char nama_peminjam[20];
+};
+
+void menambahkan_data_buku();
+void melihat_daftar_buku();
+void peminjaman_buku();
+void pengembalian_buku();
+
+int main (){
+    int pilihan;
+    cout<<"=========================\n";
+    cout<<"    Lentera Digital\n";
+    cout<<"==========================\n";
+    
+}
+    
+void peminjaman_buku(){
+    
 }
