b77a909c8e480932b56e6b82724f54c34e06bbd3
musdalifahzahra <124250135@student.upnyk.ac.id>
2026-04-08 18:07:45 +0700

    add data peminjaman ke file data_peminjaman.txt

--- Projek_Algo.cpp
+++ Projek_Algo.cpp
@@ -2,43 +2,77 @@
 #include <iomanip>
 using namespace std;
 
-struct mus{
+struct mus
+{
     ////musdal
     ////
     ////
     ////
-    ///hhhhhbsaahhhhhh
+    /// hhhhhbsaahhhhhh
 };
 
-struct dataBuku{
+struct dataBuku
+{
     char id_buku[10];
     char judul_buku[100];
     int stok_buku;
     int total_peminjaman;
     int tersedia;
     int terpinjam;
-    
-}; dataBuku buku[100];
+};
+dataBuku buku[100];
 
-struct peminjamanBuku{
+struct peminjamanBuku
+{
     char id_buku[10];
     char id_peminjam[10];
     char nama_peminjam[20];
 };
+
+peminjamanBuku v_peminjamanBuku[100];
 
 void menambahkan_data_buku();
 void melihat_daftar_buku();
 void peminjaman_buku();
 void pengembalian_buku();
 
-int main (){
+int main()
+{
     int pilihan;
-    cout<<"=========================\n";
-    cout<<"    Lentera Digital\n";
-    cout<<"==========================\n";
-    
+    cout << "=========================\n";
+    cout << "    Lentera Digital\n";
+    cout << "==========================\n";
 }
-    
-void peminjaman_buku(){
-    
+
+void peminjaman_buku()
+{
+    // buka file
+    FILE *fp_data_peminjaman = fopen("data_peminjaman.txt", "a");
+    if (!fp_data_peminjaman)
+    {
+        cout << "Belum ada data peminjaman buku";
+        return;
+    }
+
+    cout << "===== PEMINJAMAN BUKU =====" << endl;
+    cout << "ID Anggota  : ";
+    cin >> v_peminjamanBuku[0].id_peminjam;
+    cout << endl
+         << "Nama Anggota:";
+    cin.ignore();
+    cin.getline(v_peminjamanBuku[0].nama_peminjam, 20);
+    cout << endl
+         << "ID Buku     : ";
+    cin >> v_peminjamanBuku[0].id_buku;
+    cout << endl;
+
+    // menulis data peminjam ke file data_peminjaman.txt
+    fprintf(fp_data_peminjaman,
+            "%s| %s| %s|",
+            v_peminjamanBuku[0].id_peminjam,
+            v_peminjamanBuku[0].nama_peminjam,
+            v_peminjamanBuku[0].id_buku);
+
+    cout << "Data peminjaman berhasil disimpan!" << endl;
+    fclose(fp_data_peminjaman);
 }
